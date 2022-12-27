7f559b52158f7a6d4fa02a657623dde750fb221a
Ayub <75487903+ayub-a@users.noreply.github.com>
2022-12-27 17:02:17 +0300

    add mogoose with bacic files, no logic

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { TrackModule } from './track/track.module'
 
-@Module({})
+@Module({
+  imports: [
+    TrackModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://mongo-music:AidJfpFiHESAQzUU@cluster0.xbebmoq.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
+})
 export class AppModule {}
---
+++ server/src/track/shemas/comment.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import mongoose, { HydratedDocument, mongo } from 'mongoose'
+import { Track } from './track.schema'
+
+export type CommentDocument = HydratedDocument<Comment>
+
+@Schema()
+export class Comment {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Track' })
+  track: Track
+
+  @Prop()
+  username: string
+
+  @Prop()
+  text: string
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment)
