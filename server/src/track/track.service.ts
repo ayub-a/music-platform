6f95e03473b7c7eb30269e92d3ecd4b1c235a35a
Ayub <75487903+ayub-a@users.noreply.github.com>
2022-12-27 18:57:50 +0300

    CRUD without update

--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model, ObjectId } from 'mongoose'
+import { CreateTrackDto } from './dto/create-track.dto'
+import { Comment, CommentDocument } from './shemas/comment.schema'
+import { Track, TrackDocument } from './shemas/track.schema'
 
 @Injectable()
 export class TrackService {
-  async create() {}
+  constructor(
+    @InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+    @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
+  ) {}
 
-  async getAll() {}
+  async create(dto: CreateTrackDto): Promise<Track> {
+    const track = await this.trackModel.create({ ...dto, listens: 0 })
+    return track
+  }
 
-  async getOne() {}
+  async getAll(): Promise<Track[]> {
+    return await this.trackModel.find()
+  }
 
-  async delete() {}
+  async getOne(id: ObjectId): Promise<Track> {
+    return await this.trackModel.findById(id)
+  }
+
+  async delete(id: ObjectId): Promise<ObjectId> {
+    const track = await this.trackModel.findByIdAndDelete(id)
+    return track.id
+  }
 }
